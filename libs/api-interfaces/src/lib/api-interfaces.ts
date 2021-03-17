138be73a799866aa63dc5beca4d6d909a763271c
Abdulbasit <abuamer04@hotmail.com>
2021-03-17 21:23:36 +0100

    Del_1

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,16 @@
 export interface Message {
   message: string;
 }
+export interface BaseEntity {
+  Id: string | null;
+}
+export interface Widget extends BaseEntity{
+  title: string;
+  description: string;
+}
+export interface Person extends BaseEntity{
+  name: string;
+  gender: string;
+  age: number;
+}
+

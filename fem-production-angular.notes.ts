7b287c240438775daaee6d561f71fa65a664fa36
Abdulbasit <abuamer04@hotmail.com>
2021-03-31 20:10:01 +0200

    notes.ts added

---
+++ fem-production-angular.notes.ts
@@ -0,0 +1,24 @@
+npx create-nx-workspace@10.3.2 fem-production-angular \
+--appName=dashboard \
+--preset=angular-nest \
+--npmScope=fem \
+--linter=tslint \
+--nx-cloud=false \
+--style=scss
+
+//****************Libs********************** */
+nx g lib core-data --parent-module=apps/dashboard/src/app/app.module.ts --routing --style=scss && \
+nx g lib core-state --parent-module=apps/dashboard/src/app/app.module.ts --routing --style=scss && \
+nx g lib material --parent-module=apps/dashboard/src/app/app.module.ts --routing --style=scss && \
+
+//*****************************************
+nx g s services/widgets/widgets --project=core-data && \
+nx g m routing --flat=true -m=app.module.ts && \
+
+
+//*****************************************
+nx g c widgets -m app.module.ts --style=scss && \
+nx g c widgets/widgets-list -m app.module.ts --style=scss && \
+nx g c widgets/widget-details -m app.module.ts --style=scss && \
+nx g c home -m app.module.ts --style=scss
+
